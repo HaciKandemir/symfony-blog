062a136bbe9ef315a8801c8905e87fc36bec908b
HaciKandemir <hacikandemir2000@hotmail.com>
2021-03-09 20:12:57 +0300

    Rollere göre erişim engelleme eklendi

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,6 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        //$this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('home/index.html.twig');
     }
 }
